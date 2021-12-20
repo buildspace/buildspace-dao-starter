dd631b053322e5a7b8326b847b1a0edaf670faab
edcohen08 <edcohen@seas.upenn.edu>
2021-12-19 19:09:00 -0500

    adding connect to wallet button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useMemo, useState } from "react";
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to TreeDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+        Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
