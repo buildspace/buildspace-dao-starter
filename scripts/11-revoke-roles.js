459efd248d5193eac4a7b632037f1c9aca980e10
edcohen08 <edcohen@seas.upenn.edu>
2021-12-24 02:28:41 -0500

    revoking roles from my wallet

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,29 @@
+import sdk from "./1-initialize-sdk.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const tokenModule = sdk.getTokenModule(
+    process.env.TOKEN_MODULE_ADDRESS,
+);
+
+(async () => {
+  try {
+    // Log the current roles.
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
