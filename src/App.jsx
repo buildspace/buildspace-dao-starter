fba46cfe91385b7547eb633863b60369692b28d2
edcohen08 <edcohen@seas.upenn.edu>
2021-12-24 02:00:04 -0500

    rechanging logo to tree

--- src/App.jsx
+++ src/App.jsx
@@ -159,7 +159,7 @@ const App = () => {
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1>🍪DAO Member Page</h1>
+        <h1>🌳DAO Member Page</h1>
         <p>Congratulations on being a member</p>
         <div>
           <div>
