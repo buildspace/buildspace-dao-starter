0d01b85ba6d54de7b99631b85d361bb54ca770e7
Emeka <emekaokaforer@gmail.com>
2021-12-27 17:44:11 +0100

    Display members

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { useState, useEffect, useMemo } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import dotenv from "dotenv";
@@ -7,7 +8,10 @@ dotenv.config();
 const sdk = new ThirdwebSDK("rinkeby");
 
 const bundleDropModule = async () =>
-  await sdk.getBundleDropModule("0xcd64f23b79a73327abc12b789e5e9fe0866fbd21");
+  await sdk.getBundleDropModule("0xCd64f23b79A73327abc12b789E5E9FE0866FBd21");
+
+const tokenModule = async () =>
+  await sdk.getTokenModule("0xc15102395110E67b8629ebeBACB5192849eAE6B8");
 
 const App = () => {
   const { connectWallet, address, provider, error } = useWeb3();
@@ -15,7 +19,66 @@ const App = () => {
   const [hasClaimedNFT, setHasClaimedNFT] = useState();
   const [isClaiming, setIsClaiming] = useState(false);
 
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+  };
+
   const signer = provider ? provider.getSigner() : undefined;
+
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    const addressesSetter = async () => {
+      const module = await bundleDropModule();
+      return await module
+        .getAllClaimerAddresses("0")
+        .then((addresses) => {
+          console.log("🚀 Members addresses", addresses);
+          setMemberAddresses(addresses);
+        })
+        .catch((error) => {
+          console.error("failed to get member list", error);
+        });
+    };
+    return addressesSetter();
+  }, [hasClaimedNFT]);
+
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    const tokenAmountSetter = async () => {
+      const module = await tokenModule();
+      return await module
+        .getAllHolderBalances()
+        .then((amounts) => {
+          console.log("👜 Amounts", amounts);
+          setMemberTokenAmounts(amounts);
+        })
+        .catch((error) => {
+          console.error("failed to get token amounts", error);
+        });
+    };
+    return tokenAmountSetter();
+  }, [hasClaimedNFT]);
+
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          memberTokenAmounts[address] || 0,
+          18
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
     if (!address) {
@@ -59,19 +122,41 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to RealEstateDAO</h1>
+        <h1>Welcome to 🏠DAO</h1>
         <button onClick={() => connectWallet("injected")} className="button">
           Connect your wallet
         </button>
       </div>
     );
   }
-
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1>RealEstateDAO Member page</h1>
+        <h1>🏠DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
@@ -95,7 +180,7 @@ const App = () => {
 
   return (
     <div className="mint-nft">
-      <h1>Mint your free RealEstateDAO Membership NFT</h1>
+      <h1>Mint your free 🏠DAO Membership NFT</h1>
       <button disabled={isClaiming} onClick={() => mintNft()}>
         {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
       </button>
