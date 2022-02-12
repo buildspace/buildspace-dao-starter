22de5e89ecde51991536cbff3efe83f515fb1aa5
Eric Tsang <eric@m1.local>
2022-02-12 12:42:23 +0800

    section2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  // Use the connectWallet hook thirdweb gives us.
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to ReferralDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+  
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
 };
 
 export default App;
