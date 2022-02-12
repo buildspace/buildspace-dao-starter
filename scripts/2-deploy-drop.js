b791b7bb5b934d759935cc39c16b0b4077296cfe
Eric Tsang <eric@m1.local>
2022-02-12 14:00:41 +0800

    added dao image

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -2,21 +2,25 @@ import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
 
-const app = sdk.getAppModule("INSERT_YOUR_APP_ADDRESS_HERE");
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = sdk.getAppModule(`${process.env.THIRDWEB_APP_ADDRESS}`);
 
 (async () => {
   try {
     const bundleDropModule = await app.deployBundleDropModule({
       // The collection's name, ex. CryptoPunks
-      name: "NarutoDAO Membership",
+      name: "ReferralDAO Membership",
       // A description for the collection.
-      description: "A DAO for fans of Naruto.",
+      description: "A DAO for referrals to happen.",
       // The image for the collection that will show up on OpenSea.
-      image: readFileSync("scripts/assets/naruto.png"),
+      image: readFileSync("scripts/assets/ReferralDAO.png"),
       // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the module.
       // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
       // you can set this to your own wallet address if you want to charge for the drop.
-      primarySaleRecipientAddress: ethers.constants.AddressZero,
+      //   primarySaleRecipientAddress: ethers.constants.AddressZero,
+      primarySaleRecipientAddress: `${process.env.RECIPIENT_ADDRESS}`,
     });
     
     console.log(
