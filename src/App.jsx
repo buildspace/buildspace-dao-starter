4fcd9ab23e6cdcc52a8ac2c96ae13ed7cfcef99e
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-04 21:53:41 +0700

    feat: landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
+import { useAddress, useEditionDrop, useMetamask } from '@thirdweb-dev/react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   // Use the hooks thirdweb give us.
@@ -6,18 +7,75 @@ const App = () => {
   const connectWithMetamask = useMetamask();
   console.log('👋 Address:', address);
 
+  const editionDrop = useEditionDrop('0x54370BF2e0747138864A03E3B201096C233ade54');
+
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  const mintNft = async () => {
+    try {
+      setIsClaiming(true);
+      await editionDrop.claim('0', 1);
+      console.log(
+        `🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`
+      );
+      setHasClaimedNFT(true);
+    } catch (error) {
+      setHasClaimedNFT(false);
+      console.error('Failed to mint NFT', error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+    const checkBalance = async () => {
+      try {
+        const balance = await editionDrop.balanceOf(address, 0);
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log('🌟 this user has a membership NFT!');
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      } catch (error) {
+        setHasClaimedNFT(false);
+        console.error('Failed to get balance', error);
+      }
+    };
+    checkBalance();
+  }, [address, editionDrop]);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to MechKeysDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🍪DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="landing">
-      {address ? (
-        <h1>👀 wallet connected, now what!</h1>
-      ) : (
-        <div>
-          <h1>Welcome to MechkeysDAO</h1>
-          <button onClick={connectWithMetamask} className="btn-hero">
-            Connect your wallet
-          </button>
-        </div>
-      )}
+    <div className="mint-nft">
+      <h1>Mint your free 🎹 DAO Membership NFT</h1>
+      <button disabled={isClaiming} onClick={mintNft}>
+        {isClaiming ? 'Minting...' : 'Mint your nft (FREE)'}
+      </button>
     </div>
   );
 };
