19db03fcfe9891113450d0f408d9d457b8484a8d
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-04 17:14:01 +0700

    feat: connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+  // Use the hooks thirdweb give us.
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log('👋 Address:', address);
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      {address ? (
+        <h1>👀 wallet connected, now what!</h1>
+      ) : (
+        <div>
+          <h1>Welcome to MechkeysDAO</h1>
+          <button onClick={connectWithMetamask} className="btn-hero">
+            Connect your wallet
+          </button>
+        </div>
+      )}
     </div>
   );
 };
