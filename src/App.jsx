7fc94af535e1013d98a9ea89dd2da51632075db6
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-04 23:07:15 +0700

    feat: show memberList (addresses, balances)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useAddress, useEditionDrop, useMetamask } from '@thirdweb-dev/react';
-import { useState, useEffect } from 'react';
+import { useToken, useAddress, useEditionDrop, useMetamask } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
   // Use the hooks thirdweb give us.
@@ -11,6 +11,59 @@ const App = () => {
 
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   const [isClaiming, setIsClaiming] = useState(false);
+
+  const token = useToken('0xFd57fa6994E0b1907777E6b03371828Db631F281');
+
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + '...' + str.substring(str.length - 4);
+  };
+
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    const getAllAddresses = async () => {
+      try {
+        const addresses = await editionDrop.history.getAllClaimerAddresses(0);
+        setMemberAddresses(addresses);
+        console.log('🚀 Members addresses', addresses);
+      } catch (err) {
+        console.error('Failed to get member addresses list: ', err);
+      }
+    };
+
+    getAllAddresses();
+  }, [hasClaimedNFT, editionDrop.history]);
+
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    const getAllBalances = async () => {
+      try {
+        const amounts = await token.history.getAllHolderBalances();
+        setMemberTokenAmounts(amounts);
+        console.log('👜 Amounts', amounts);
+      } catch (err) {
+        console.error('Failed to get balances: ', err);
+      }
+    };
+
+    getAllBalances();
+  }, [hasClaimedNFT, token.history]);
+
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      const member = memberTokenAmounts?.find(({ holder }) => holder === address);
+
+      return { address, tokenAmount: member?.balance.displayValue || '0' };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   const mintNft = async () => {
     try {
@@ -66,6 +119,29 @@ const App = () => {
       <div className="member-page">
         <h1>🍪DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
