a39cca56352826242abff241ca6c119dba9ca261
Dmitrii Goncharov <dmitrii@Dmitriis-MacBook-Air.local>
2022-06-06 22:57:13 +0200

    Introduce wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address: ", address);
+  
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to this DAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+    </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-// Import thirdweb provider and Rinkeby ChainId
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-// This is the chainId your dApp will work on.
 const activeChainId = ChainId.Rinkeby;
 
-// Wrap your app with the thirdweb provider
 ReactDOM.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
