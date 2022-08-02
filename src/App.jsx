0b0c83317f7f4283544ca49b13110d9f6da90345
johnpagtama <pagtamajr@gmail.com>
2022-08-01 20:06:29 -0700

    Deployed NFT bundle and metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
-
 const App = () => {
-	const address = useAddress();
-	const connectWithMetamask = useMetamask();
-
-	console.log(`👋 Address: ${address}`);
-
-	if (!address) {
-		return (
-			<div className='landing'>
-				<h1>Welcome to UserDAO</h1>
-				<button onClick={connectWithMetamask}>
-					Connect Wallet
-				</button>
-			</div>
-		);
-	}
-
-	return (
-		<div className='landing'>
-			<h1>👀 Wallet Connected</h1>
-		</div>
-	);
+  return (
+    <div className="landing">
+      <h1>Welcome to My DAO</h1>
+    </div>
+  );
 };
 
 export default App;
