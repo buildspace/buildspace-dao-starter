22b1fce0cd22aba79965e0a1ca6951e3bbdf08b5
David Meade Jr <davidmeadejnr@gmail.com>
2022-09-16 20:58:41 +0100

    Connect wallet button functionality added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋🏾 Address", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1> Welcome to BuildditDAO 🧱🤝🌐</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected 🔗, LFG 🚀 </h1>
     </div>
   );
 };
