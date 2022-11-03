defd58247c20d1246f081b5ea00f56709e8f9bd0
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-11-03 22:52:54 +0100

    Update index.js for React 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
@@ -11,11 +11,12 @@ import { ChainId } from '@thirdweb-dev/sdk';
 const activeChainId = ChainId.Goerli;
 
 // Wrap your app with the thirdweb provider
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
