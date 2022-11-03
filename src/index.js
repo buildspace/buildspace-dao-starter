4df86edba28e8895f7f2d9f21455014e3d6c067e
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-11-03 20:49:05 +0100

    Update starter to v3

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// Import thirdweb provider and Rinkeby ChainId
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+// Import thirdweb provider and Goerli ChainId
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import { ChainId } from '@thirdweb-dev/sdk';
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Rinkeby;
+const activeChainId = ChainId.Goerli;
 
 // Wrap your app with the thirdweb provider
 ReactDOM.render(
