ad3e87dd986eaaf93fd8f753ce243d15508acc25
jia-wei-teh <jwteh5695@hotmail.com>
2022-12-07 00:03:10 +0800

    connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
+
 const App = () => {
+
+  const address = useAddress();
+  console.log("Wallet address is", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to RunningDAO</h1>
+        <div className="btn-hero">
+          <ConnectWallet />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
+
 };
 
 export default App;
