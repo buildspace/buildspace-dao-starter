11e71d276d56c8edb41d5a336705a9fc2ea10813
jia-wei-teh <jwteh5695@hotmail.com>
2022-12-12 01:10:03 +0800

    vote proposal

---
+++ scripts/10-create-vote-proposals.js
@@ -0,0 +1,75 @@
+import sdk from "./1-initialize-sdk.js";
+import { ethers } from "ethers";
+
+(async () => {
+  try {
+    // This is our governance contract.
+    const vote = await sdk.getContract(process.env.VOTING_CONTRACT_ADDRESS, "vote");
+    // This is our ERC-20 contract.
+    const token = await sdk.getContract(process.env.TOKEN_ADDRESS, "token");
+    // Create proposal to mint 420,000 new token to the treasury.
+    const amount = 420_000;
+    const description = "Should the DAO mint an additional " + amount + " tokens into the treasury?";
+    const executions = [
+      {
+        // Our token contract that actually executes the mint.
+        toAddress: token.getAddress(),
+        // Our nativeToken is ETH. nativeTokenValue is the amount of ETH we want
+        // to send in this proposal. In this case, we're sending 0 ETH.
+        // We're just minting new tokens to the treasury. So, set to 0.
+        nativeTokenValue: 0,
+        // We're doing a mint! And, we're minting to the vote, which is
+        // acting as our treasury.
+        // in this case, we need to use ethers.js to convert the amount
+        // to the correct format. This is because the amount it requires is in wei.
+        transactionData: token.encoder.encode(
+          "mintTo", [
+          vote.getAddress(),
+          ethers.utils.parseUnits(amount.toString(), 18),
+        ]
+        ),
+      }
+    ];
+
+    await vote.propose(description, executions);
+
+    console.log("✅ Successfully created proposal to mint tokens");
+  } catch (error) {
+    console.error("Failed to create first proposal", error);
+    process.exit(1);
+  }
+
+  try {
+    // This is our governance contract.
+    const vote = await sdk.getContract(process.env.VOTING_CONTRACT_ADDRESS, "vote");
+    // This is our ERC-20 contract.
+    const token = await sdk.getContract(process.env.TOKEN_ADDRESS, "token");
+    // Create proposal to transfer ourselves 6,900 tokens for being awesome.
+    const amount = 6_900;
+    const description = "Should the DAO transfer " + amount + " tokens from the treasury to " +
+      process.env.WALLET_ADDRESS + " for being awesome?";
+    const executions = [
+      {
+        // Again, we're sending ourselves 0 ETH. Just sending our own token.
+        toAddress: token.getAddress(),
+        nativeTokenValue: 0,
+        transactionData: token.encoder.encode(
+          // We're doing a transfer from the treasury to our wallet.
+          "transfer",
+          [
+            process.env.WALLET_ADDRESS,
+            ethers.utils.parseUnits(amount.toString(), 18),
+          ]
+        ),
+      },
+    ];
+
+    await vote.propose(description, executions);
+
+    console.log(
+      "✅ Successfully created proposal to reward ourselves from the treasury, let's hope people vote for it!"
+    );
+  } catch (error) {
+    console.error("Failed to create second proposal", error);
+  }
+})();
---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,22 @@
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    const token = await sdk.getContract(process.env.TOKEN_ADDRESS, "token");
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO trasury", error);
+  }
+})();
