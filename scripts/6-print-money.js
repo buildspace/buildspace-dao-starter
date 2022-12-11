ca2cdf7da4354db36608f4c9b3f0e891b55c160b
jia-wei-teh <jwteh5695@hotmail.com>
2022-12-12 01:09:31 +0800

    create treasury

---
+++ scripts/6-print-money.js
@@ -0,0 +1,18 @@
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    // This is the address of our ERC-20 contract printed out in the step before.
+    const token = await sdk.getContract("0xEE5a1e5A855dc8DCb9d98dD0f9a8cda93a64aC42", "token");
+    // What's the max supply you want to set? 1,000,000 is a nice number!
+    const amount = 1_000_000;
+    // Interact with your deployed ERC-20 contract and mint the tokens!
+    await token.mint(amount);
+    const totalSupply = await token.totalSupply();
+
+    // Print out how many of our token's are out there now!
+    console.log("✅ There now is", totalSupply.displayValue, "$RUN in circulation");
+  } catch (error) {
+    console.error("Failed to print money", error);
+  }
+})();
