58f488bba7adb2f24cc6c47ca2b89f3afddbe726
Ariel Segura <ariel.segura@yahoo.com>
2022-12-14 23:44:05 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// Import thirdweb provider and Goerli ChainId
+// Import thirdweb provider and Mumbai ChainId
 import { ThirdwebProvider } from '@thirdweb-dev/react';
-import { ChainId } from '@thirdweb-dev/sdk';
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Goerli;
+const activeChainId = 80001;
 
 // Wrap your app with the thirdweb provider
 const container = document.getElementById('root');
